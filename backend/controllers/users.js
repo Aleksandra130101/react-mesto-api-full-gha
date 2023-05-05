629bc7b19dce7250f747754918b702c7fa006011
Aleksandra130101 <104098142+Aleksandra130101@users.noreply.github.com>
2023-05-06 01:31:44 +0300

    Add files via upload

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -88,7 +88,7 @@ module.exports.login = (req, res, next) => {
         maxAge: 3600000,
         httpOnly: true,
       });
-      res.send({ message: 'Вы успешно вошли!', token });
+      res.send({ message: 'Вы успешно вошли!' });
     })
     .catch((err) => {
       next(err);
